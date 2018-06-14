bc5868cfcebb0fb0a68911441846dbb0c1b2bc7f
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-06-14 14:50:18 -0400

    Using Trigger Volumes

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -19,16 +19,18 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
+void UOpenDoor::OpenDoor()
+{
 	// Find the owning Actor
 	AActor* Owner = GetOwner();
 
 	// Create a rotator
 	FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
-	
+
 	// Set the door rotation
 	Owner->SetActorRotation(NewRotation);
-	
 }
 
 
@@ -37,6 +39,14 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Poll the Trigger Volume
+	// If ActorThatOpens is in the volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+
+	}
+	
+	
 }
 
--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -21,6 +21,8 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	void OpenDoor();
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -35,4 +37,7 @@ private:
 	// Same as [SerializeField] in Unity
 	UPROPERTY(EditAnywhere)
 	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	AActor* ActorThatOpens; // Pawn inherits from Actor
 };
